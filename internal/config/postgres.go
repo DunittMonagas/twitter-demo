2e48bb099f39a4a3e491bf79f3c99c28d7313f29
Dunitt Monagas <8442719+DunittMonagas@users.noreply.github.com>
2026-02-02 05:04:42 -0300

    Add Postgres configuration and connection handling
    - Implement PostgresConfig struct to manage database connection settings.
    - Create NewPostgresConfig function to initialize configuration from environment variables.
    - Add Postgres struct and NewPostgres function for establishing a connection to the Postgres database.
    - Include error handling for connection and ping operations to ensure reliability.

--- internal/config/postgres.go
+++ internal/config/postgres.go
@@ -1,1 +1,37 @@
 package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+)
+
+const (
+	PostgresDriver = "postgres"
+)
+
+type PostgresConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Database string
+	Driver   string
+}
+
+func NewPostgresConfig() PostgresConfig {
+
+	port, err := strconv.Atoi(os.Getenv("POSTGRES_PORT"))
+	if err != nil {
+		log.Fatalf("Failed to convert POSTGRES_PORT to int: %v", err)
+	}
+
+	return PostgresConfig{
+		Host:     os.Getenv("POSTGRES_HOST"),
+		Port:     port,
+		User:     os.Getenv("POSTGRES_USER"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+		Database: os.Getenv("POSTGRES_DB"),
+		Driver:   PostgresDriver,
+	}
+}
--- pkg/postgres.go
+++ pkg/postgres.go
@@ -1,1 +1,31 @@
 package pkg
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"twitter-demo/internal/config"
+)
+
+type Postgres struct {
+	*sql.DB
+}
+
+func NewPostgres(config config.PostgresConfig) (*Postgres, error) {
+
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Host, config.Port, config.User, config.Password, config.Database)
+
+	db, err := sql.Open(config.Driver, dsn)
+	if err != nil {
+		log.Fatalf("Failed to connect to Postgres: %v", err)
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Failed to ping Postgres: %v", err)
+		return nil, err
+	}
+
+	return &Postgres{DB: db}, nil
+}
