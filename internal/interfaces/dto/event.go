b11dec7a77fa701a63dc38b9ec0e2ddedc5a0212
Dunitt Monagas <8442719+DunittMonagas@users.noreply.github.com>
2026-02-03 13:59:35 -0300

    Remove unused event types and their associated data structures from event DTOs to streamline the event handling code.

--- internal/interfaces/dto/event.go
+++ internal/interfaces/dto/event.go
@@ -8,18 +8,6 @@ type EventType string
 const (
 	// TweetCreatedEvent is published when a new tweet is created
 	TweetCreatedEvent EventType = "tweet.created"
-
-	// TweetUpdatedEvent is published when a tweet is updated
-	TweetUpdatedEvent EventType = "tweet.updated"
-
-	// TweetDeletedEvent is published when a tweet is deleted
-	TweetDeletedEvent EventType = "tweet.deleted"
-
-	// FollowEvent is published when a user follows another user
-	FollowEvent EventType = "follow.created"
-
-	// UnfollowEvent is published when a user unfollows another user
-	UnfollowEvent EventType = "follow.deleted"
 )
 
 // Event is a generic event wrapper for all domain events
@@ -36,24 +24,6 @@ type TweetCreatedEventData struct {
 	UserID    int64     `json:"user_id"`
 	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"created_at"`
-}
-
-// TweetUpdatedEventData contains the data for a tweet.updated event
-type TweetUpdatedEventData struct {
-	TweetID   int64     `json:"tweet_id"`
-	Content   string    `json:"content"`
-	UpdatedAt time.Time `json:"updated_at"`
-}
-
-// TweetDeletedEventData contains the data for a tweet.deleted event
-type TweetDeletedEventData struct {
-	TweetID int64 `json:"tweet_id"`
-}
-
-// FollowEventData contains the data for follow/unfollow events
-type FollowEventData struct {
-	FollowerID int64 `json:"follower_id"`
-	FollowedID int64 `json:"followed_id"`
 }
 
 // NewEvent creates a new Event with the current timestamp
